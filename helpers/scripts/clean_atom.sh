fe66dde23bb6aa8258d2c1e996c09c32c44b0bcf
Ceco <ceco@cityos.io>
2017-01-24 01:43:36 +0100

    new script

--- helpers/scripts/clean_atom.sh
+++ helpers/scripts/clean_atom.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 rm -rf ~/.atom/
-rm  /usr/local/bin/atom
-rm /usr/local/bin/apm
+rm -rf ~/.platformio/
+rm -rf /usr/local/bin/atom
+rm -rf /usr/local/bin/apm
 rm ~/Library/Preferences/com.github.atom.plist
