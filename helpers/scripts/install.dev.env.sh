005bd35190e2da71803201046946842a10362ef5
Ceco <ceco@cityos.io>
2017-01-24 03:19:48 +0100

    Added drivers and Desktop

--- helpers/scripts/install.dev.env.sh
+++ helpers/scripts/install.dev.env.sh
@@ -4,12 +4,10 @@ mkdir ~/src/
 cd ~/src/
 git clone https://github.com/appsmonkey/air.nodemcu.full.git
 
-#curl -L --silent "https://github.com/appsmonkey/air.nodemcu.full/archive/master.zip" --output air.nodemcu.full.zip
-#unzip air.nodemcu.full.zip
-#rm -f air.nodemcu.full.zip
-
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew cask install Caskroom/cask/atom
+brew cask install Caskroom/cask/github-desktop
+brew cask install Caskroom/cask/silicon-labs-vcp-driver
 brew install uncrustify
 
 apm install platformio-ide
@@ -20,3 +18,7 @@ cp ~/src/air.nodemcu.full/helpers/config.cson ~/.atom/
 
 cd ./air.nodemcu.full/
 git update-index --assume-unchanged lib/Config/Config.h
+
+#curl -L --silent "https://github.com/appsmonkey/air.nodemcu.full/archive/master.zip" --output air.nodemcu.full.zip
+#unzip air.nodemcu.full.zip
+#rm -f air.nodemcu.full.zip
