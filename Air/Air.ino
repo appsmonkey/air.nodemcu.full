525665ba4a7532748373f87e116f595ae61ed679
Ceco <ceco@cityos.io>
2018-01-21 14:21:46 +0100

    Dev example settings - clean main

--- Air/Air.ino
+++ Air/Air.ino
@@ -3,18 +3,7 @@
 
 // setup runs once
 void setup()
-{
-    // ctos.sensing.interval = 5;
-    // ring.listen = "air aqi range";
-    // ring.debug  = true;
-
-    ctos.debug.errors = true;
-    // ctos.debug.wifi     = true;
-    // ctos.debug.api    = true;
-    ctos.debug.senses = true;
-    // ctos.debug.controls = true;
-    // ctos.debug.schema   = true;
-}
+{ }
 
 // runs over and over again
 void loop()
