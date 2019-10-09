1f3523cea14925444f9ca08599a81bb0c01daa73
dzavid <dzavid.isanovic@gmail.com>
2019-10-09 16:27:10 +0200

    Added support for SCD30
    Added support for Sparkfun's SCD30 CO2 sensor

---
+++ lib/Sense/SCD_30/SCD_30.h
@@ -0,0 +1,34 @@
+#ifndef SCD_30_H
+#define SCD_30_H
+
+/***************************************************************************
+*  SCD30 co2, humidity & temperature
+*  These sensors use I2C 0x61 address.
+***************************************************************************/
+
+#include "CityOS.h"
+
+#include <Wire.h>
+#include <SPI.h>
+#include <SparkFun_SCD30_Arduino_Library.h>
+
+class SCD_30 : public CityOS {
+public:
+
+    SCD_30(int scl, int sda);
+    SCD_30(int scl, int sda, int i2c);
+    ~SCD_30();
+
+    struct _in {
+        // SCD_30 Sensor
+        int co2;
+    } in;
+
+    void interval();
+    private:
+    SCD30 * _scd;
+    TwoWire * _wire;
+    void setup(int scl, int sda, int i2c);
+};
+
+#endif /* ifndef SCD_30_H */
